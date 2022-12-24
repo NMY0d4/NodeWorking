624b6d64c60cb1f93025e57200a19b1b9523c653
Grégory Marini <greg.marini@hotmail.fr>
2022-12-24 16:18:08 +0100

    create new folder and installing axios for understanding how it works

---
+++ package-example/request.js
@@ -0,0 +1,7 @@
+const axios = require("axios");
+
+axios
+    .get("https://www.google.com")
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err.message))
+    .then(() => console.log("All done"));
