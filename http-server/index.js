5899065dcaa428295e6569c4c275add7ec5c037e
Grégory Marini <greg.marini@hotmail.fr>
2022-12-26 18:01:22 +0100

    add pipe in POST method To have the data confirmed in RES

--- http-server/index.js
+++ http-server/index.js
@@ -28,6 +28,7 @@ const server = http.createServer((req, res) => {
             console.log("Request:", friend);
             friends.push(JSON.parse(friend));
         });
+        req.pipe(res);
     } else if (req.method === "GET" && items[1] === "friends") {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
