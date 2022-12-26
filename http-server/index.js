649e3c30c1bd6a7be0804d5131f9b9788d706a5b
Grégory Marini <greg.marini@hotmail.fr>
2022-12-26 16:05:33 +0100

    learning on node basics

--- http-server/index.js
+++ http-server/index.js
@@ -2,20 +2,32 @@ const http = require("http");
 
 const PORT = 3000;
 
+const friends = [
+    {
+        id: 0,
+        name: "Nikola tesla",
+    },
+    {
+        id: 1,
+        name: "Sir Isaac Newton",
+    },
+    {
+        id: 2,
+        name: "Albert Einstein",
+    },
+];
+
 const server = http.createServer((req, res) => {
-    if (req.url === "/testURL") {
-        // res.writeHead(200, {
-        //     "Content-type": "application/json",
-        // });
+    const items = req.url.split("/");
+    if (items[1] === "friends") {
         res.statusCode = 200;
         res.setHeader("Content-Type", "application/json");
-        res.end(
-            JSON.stringify({
-                id: 1,
-                name: "Sir Isaac Newton",
-            })
-        );
-    } else if (req.url === "/test2") {
+        if (items.length === 3) {
+            res.end(JSON.stringify(friends[+items[2]]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (items[1] === "messages") {
         res.setHeader("Content-Type", "text/html");
         res.write(
             "<html><body><ul><li>What are your thoughts on astronomy?</li><li>Hello Isaac</li></ul></body></html>"
