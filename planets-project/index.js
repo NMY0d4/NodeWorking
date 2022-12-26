9e14b283ce56b2009e4b758ced11247a31e4f6a6
Grégory Marini <greg.marini@hotmail.fr>
2022-12-26 10:20:50 +0100

    filtering planets results

--- planets-project/index.js
+++ planets-project/index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return (
+        planet["koi_disposition"] === "CONFIRMED" &&
+        planet["koi_insol"] > 0.36 &&
+        planet["koi_insol"] < 1.11 &&
+        planet["koi_prad"] < 1.6
+    );
+}
 
 fs.createReadStream("kepler_data.csv")
     .pipe(
@@ -11,14 +20,16 @@ fs.createReadStream("kepler_data.csv")
         })
     )
     .on("data", (data) => {
-        results.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on("error", (err) => {
         console.log(err);
     })
     .on("end", () => {
-        console.log(results);
-        console.log("done");
+        console.log(habitablePlanets);
+        console.log(`${habitablePlanets.length} habitable planets found!`);
     });
 
 // parse();
