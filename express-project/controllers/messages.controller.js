e8a70675d566efe6ea78684a287652920857a3f0
Grégory Marini <greg.marini@hotmail.fr>
2022-12-31 12:36:59 +0100

    add and apply hbs for template

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
 function getMessages(req, res) {
-    res.sendFile(
-        path.join(__dirname, "..", "public", "images", "skimountain.jpg")
-    );
+    // res.sendFile(
+    //     path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+    // );
+    res.render("messages", {
+        title: "Messages to my Friends!",
+        friend: "Elon Musk",
+    });
 }
 
 function postMessage(req, res) {
