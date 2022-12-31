f40cc3f775ac8a198fade1d94dfbd44c26e89963
Grégory Marini <greg.marini@hotmail.fr>
2022-12-31 09:49:32 +0100

    try getMessage with sendFile express function

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessages(req, res) {
-    res.send("<ul><li>Hello Albert!</li><li>Hello Isaac!</li></ul>");
+    res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
+    // res.send("<ul><li>Hello Albert!</li><li>Hello Isaac!</li></ul>");
 }
 
 function postMessage(req, res) {
