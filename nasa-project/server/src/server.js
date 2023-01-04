e86a200688d34e27b3ada1679b6848bed85aa970
Grégory Marini <greg.marini@hotmail.fr>
2023-01-04 14:14:54 +0100

    build our first link between client and server

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -5,4 +5,4 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`listen on ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on ${PORT}...`));
