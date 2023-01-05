ac4199a6b9ee7d6fb61f02d3666ff1dc23160395
Grégory Marini <greg.marini@hotmail.fr>
2023-01-05 11:47:52 +0100

    add deploy command ath the base of the project

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const path = require("path");
 const planetsRouter = require("./routes/planets/planets.router");
 
 const app = express();
@@ -11,6 +11,11 @@ app.use(
     })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public','index.html"));
+});
 
 module.exports = app;
