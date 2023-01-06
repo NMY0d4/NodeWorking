38c5160938cc8d0dffe3d9c5df97000bf0ffe479
Grégory Marini <greg.marini@hotmail.fr>
2023-01-06 19:11:48 +0100

    add error handling in addNewLaunch function

--- nasa-project/server/src/routes/launches/launches.controller.js
+++ nasa-project/server/src/routes/launches/launches.controller.js
@@ -7,7 +7,20 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
 
+    if (
+        !launch.mission ||
+        !launch.rocket ||
+        !launch.launchDate ||
+        !launch.destination
+    ) {
+        return res.status(400).json({
+            error: "Missing required launch property",
+        });
+    }
     launch.launchDate = new Date(launch.launchDate);
+    if (!isFinite(launch.launchDate)) {
+        return res.status(400).json({ error: "Invalid launch date" });
+    }
 
     addNewLaunch(launch);
     res.status(201).json(launch);
