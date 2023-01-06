860142971975493b50bf22efeadf2fafc25d4df4
Grégory Marini <greg.marini@hotmail.fr>
2023-01-06 15:38:34 +0100

    add post methode for launch

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -17,10 +17,10 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(planetsRouter);
-app.use(lauchesRouter);
+app.use("/planets", planetsRouter);
+app.use("/launches", lauchesRouter);
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public','index.html"));
 });
 
