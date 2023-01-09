ca2f60a2d409872595df81b554c5eb4ba88a4299
Grégory <greg.marini@hotmail.fr>
2023-01-09 16:29:45 +0100

    end of launches tests

--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -24,6 +24,13 @@ describe("Test POST /launches", () => {
     target: "Kepler-186 f",
   };
 
+  const launchDataWithInvalidDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "zoot",
+  };
+
   test("It should respond with  201 created", async () => {
     const response = await request(app)
       .post("/launches")
@@ -39,6 +46,24 @@ describe("Test POST /launches", () => {
     expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 
-  test("It should catch missing required properties", () => {});
-  test("It should catch missing invalid dates", () => {});
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+  test("It should catch missing invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({ error: "Invalid launch date" });
+  });
 });
