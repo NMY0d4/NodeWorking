bc2193e90b904e77894f77e9336db755cd054bd7
Grégory <greg.marini@hotmail.fr>
2023-01-12 16:19:45 +0100

    configure dotenv and mongoDB connection

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,16 +1,32 @@
 const http = require("http");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = require("./app");
+
+dotenv.config();
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_DB_URL;
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
-    await loadPlanetsData();
-    server.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+  await mongoose.connect(MONGO_URL);
+
+  await loadPlanetsData();
+  server.listen(PORT, () => console.log(`Listening on ${PORT}...`));
 }
 
 startServer();
