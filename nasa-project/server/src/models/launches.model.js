b1f9c67cb340e049c531da06d5c27afb36c07f84
Grégory Marini <greg.marini@hotmail.fr>
2023-01-13 21:16:18 +0100

    add function getLatestFlightNumber

--- nasa-project/server/src/models/launches.model.js
+++ nasa-project/server/src/models/launches.model.js
@@ -1,8 +1,9 @@
 const launchesDatabase = require("./launches.mongo");
+const planets = require("./planets.mongo");
+
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launches = new Map();
-
-let latestFlightNumber = 100;
 
 const launch = {
     flightNumber: 100,
@@ -21,11 +22,27 @@ function existsLaunchWithId(launchId) {
     return launches.has(launchId);
 }
 
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
+
+    if (!latestLaunch) {
+        return DEFAULT_FLIGHT_NUMBER;
+    }
+
+    return latestLaunch.flightNumber;
+}
+
 async function getAllLaunches() {
     return await launchesDatabase.find({}, { _id: 0, __v: 0 });
 }
 
 async function saveLaunch(launch) {
+    const planet = await planets.findOne({ keplerName: launch.target });
+
+    if (!planet) {
+        throw new Error("No matching planet found");
+    }
+
     await launchesDatabase.updateOne(
         {
             flightNumber: launch.flightNumber,
