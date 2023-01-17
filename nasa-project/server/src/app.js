5d71e2f66c56a501bd1b05822b576cf60a1f6708
Grégory <greg.marini@hotmail.fr>
2023-01-17 16:09:21 +0100

    refactoring in v1 api

--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const planetsRouter = require("./routes/planets/planets.router");
-const lauchesRouter = require("./routes/launches/launches.router");
 const morgan = require("morgan");
+const api = require("./routes/api");
+
 const app = express();
 
 app.use(
-    cors({
-        origin: "http://localhost:3000",
-    })
+  cors({
+    origin: "http://localhost:3000",
+  })
 );
 
 app.use(morgan("combined"));
@@ -17,11 +17,10 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", lauchesRouter);
+app.use("/v1", api);
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "public','index.html"));
+  res.sendFile(path.join(__dirname, "..", "public','index.html"));
 });
 
 module.exports = app;
