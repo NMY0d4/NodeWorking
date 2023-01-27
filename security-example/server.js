59e3ed68ed8429fdd4dd9020ee448ebdc64f5fdc
Grégory Marini <greg.marini@hotmail.fr>
2023-01-27 18:48:19 +0100

    add checkLoggedIn function

--- security-example/server.js
+++ security-example/server.js
@@ -9,22 +9,31 @@ const app = express();
 
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
-  return res.send("Your personal secret value is 42!");
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true; //TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({ error: "You must log in!" });
+    }
+
+    next();
+}
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+    return res.send("Your personal secret value is 42!");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 https
-  .createServer(
-    {
-      key: fs.readFileSync("key.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-  });
+    .createServer(
+        {
+            key: fs.readFileSync("key.pem"),
+            cert: fs.readFileSync("cert.pem"),
+        },
+        app
+    )
+    .listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
