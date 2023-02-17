02941a208804232449f49a40c1c9ac94c6a939a5
Grégory Marini <greg.marini@hotmail.fr>
2023-02-17 18:02:09 +0100

    designing an E-com shema(train)

--- GraphQL/GQLexample/server.js
+++ GraphQL/GQLexample/server.js
@@ -3,15 +3,65 @@ const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 
 const schema = buildSchema(`
+
     type Query {
-        description: String
-        price: Float
+        products: [Product]   
+        orders: [Order]     
+    }
+
+    type Product {
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-    description: "Red Shoe",
-    price: 42.12,
+    products: [
+        {
+            id: "redshoe",
+            description: "Red Shoe",
+            price: 42.12,
+        },
+        {
+            id: "bluejean",
+            description: "Blue Jeans",
+            price: 55.55,
+        },
+    ],
+    orders: [
+        {
+            date: "2005-05-05",
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: "redshoe",
+                        description: "Old Red Shoe",
+                        price: 45.11,
+                    },
+                    quantity: 2,
+                },
+            ],
+        },
+    ],
 };
 
 const app = express();
@@ -21,6 +71,7 @@ app.use(
     graphqlHTTP({
         schema: schema,
         rootValue: root,
+        graphiql: true,
     })
 );
 
