4640cc501fbddd97f0d1299ad3e2ae615f8088c7
Grégory Marini <greg.marini@hotmail.fr>
2023-02-18 15:58:29 +0100

    restructuring with graphql tools

---
+++ GraphQL/GQLexample/orders/orders.model.js
@@ -0,0 +1,18 @@
+module.exports = [
+    orders: [
+        {
+            date: "2005-05-05",
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: "redshoe",
+                        description: "Old Red Shoe",
+                        price: 45.11,
+                    },
+                    quantity: 2,
+                },
+            ],
+        },
+    ],
+];
